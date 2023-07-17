09e5e82525f81ad3ac2f9ded566f2dfeb87e5b09
Joey Francisco <joeycarlo@gmail.com>
2023-07-18 06:25:04 +0800

    Impelemented threeMedian method for picking pivot value from 1st, middle, and last element, added test cases for same

---
+++ src/test/java/main/QuickSortTest.java
@@ -0,0 +1,16 @@
+package main;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class QuickSortTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void threeMedian() {
+    }
+}

ee943ec399c29af63994fc78de39db6268bcbf29
Joey Francisco <joeycarlo@gmail.com>
2023-07-18 06:27:30 +0800

    Impelemented threeMedian method for picking pivot value from 1st, middle, and last element, added test cases for same

--- src/main/java/main/QuickSort.java
+++ src/main/java/main/QuickSort.java
@@ -1,2 +1,50 @@
-package main;public class QuickSort {
+package main;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Arrays;
+
+public class QuickSort {
+    private static final Logger logger = LoggerFactory.getLogger(QuickSort.class);
+
+    private QuickSort() {}
+    private static QuickSort instance = null;
+
+    public static QuickSort getInstance() {
+        if (instance == null)
+            return new QuickSort();
+
+        return instance;
+    }
+
+    public int[] sort(int []a, int pivot) {
+        logger.trace("Sorting array using QuickSort algorithm");
+        int input[] = a.clone();
+
+        return input;
+    }
+
+    public int threeMedian(int []a) throws NullPointerException{
+        int[] array = a.clone();
+        if (array.length == 1)
+            return array[0];
+
+        int temp;
+
+        for (int x = 0; x < array.length - 1; x++) {
+            for (int i = 0 ; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    temp = array[i + 1];
+                    array[i + 1] = array[i];
+                    array[i] = temp;
+                }
+            }
+        }
+        logger.debug("array[0]: {}", array[0]);
+        logger.debug("array[1]: {}", array[1]);
+        logger.debug("array[2]: {}", array[2]);
+        return array[1];
+    }
+
 }
--- src/test/java/main/QuickSortTest.java
+++ src/test/java/main/QuickSortTest.java
@@ -1,16 +1,43 @@
 package main;
 
+import com.sun.org.glassfish.gmbal.Description;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.function.ThrowingRunnable;
 
 import static org.junit.Assert.*;
+
 public class QuickSortTest {
+
+    QuickSort quickSort;
 
     @Before
     public void setUp() throws Exception {
+        quickSort = QuickSort.getInstance();
     }
 
     @Test
-    public void threeMedian() {
+    @Description("Should sort the array and return the middle value")
+    public void threeMedianPositiveTest() {
+        int[] input = {3, 2, 1};
+        assertEquals(2, quickSort.threeMedian(input));
+    }
+
+    @Test
+    @Description("Should throw null pointer exception when supplied with null array")
+    public void threeMedianNullTest() {
+        assertThrows(NullPointerException.class, new ThrowingRunnable() {
+            @Override
+            public void run() throws Throwable {
+                quickSort.threeMedian(null);
+            }
+        });
+    }
+
+    @Test
+    @Description("Should return same element when supplied with size 1 array")
+    public void threeMedianOneElement() {
+        int []input = { 2 };
+        assertEquals(2, quickSort.threeMedian(input));
     }
 }
